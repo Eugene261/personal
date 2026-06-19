f999ab0093f9ec44ea5072865c1cd59d4ce381ee
Eugene Opoku <eugeneopoku74@gmail.com>
2026-06-19 12:46:55 +0000

    fix: print output layout styling, hide footer/header, and disable browser header/footers

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "../components/footer";
 export default function SiteLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-      <header className="mb-16 flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
+      <header className="no-print mb-16 flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
         <Link href="/" className="font-serif text-lg tracking-tight font-medium hover:opacity-75 transition-opacity">
           Eugene Opoku
         </Link>
@@ -31,7 +31,9 @@ export default function SiteLayout({ children }: { children: React.ReactNode }) 
       </header>
       <main className="flex-auto min-w-0 flex flex-col px-2 md:px-0">
         {children}
-        <Footer />
+        <div className="no-print">
+          <Footer />
+        </div>
       </main>
     </div>
   );
